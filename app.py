e610775c28d74fcbd9efb99a5dfb0be41a795583
Joe Chen <wentsaochen@gmail.com>
2021-11-09 22:45:48 +0800

    the third app.py

--- app.py
+++ app.py
@@ -40,6 +40,18 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     r = '很抱歉,你說什麼?'
+
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=r))
+        return       
+    
     if msg in ['hi', 'Hi']:
         r = '嗨'
     elif msg == '你吃飯了沒':
@@ -51,10 +63,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-        ))
+        TextSendMessage(text=r))
             
 
 if __name__ == "__main__":
